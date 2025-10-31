f891a62c0b5467f8f9fcc7c958a35ea3c8f73867
M ABDUL AHAD <151634430+MABDULAHAD-HUB@users.noreply.github.com>
2025-10-31 15:55:41 +0530

    Fix logo path in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,7 +32,7 @@ const Header = ({ currentPage, onPageChange }) => {
         <div className="brand-section">
           <div className="brand-logo">
             <img 
-              src=/logo.svg
+              src="publlic/logo.svg"
               alt="TypingForge-Logo" 
               className="logo-svg"
               width="200"
